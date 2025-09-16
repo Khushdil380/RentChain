1310df2fbfac31481582ac3958e3cc1691997b3a
Khushdil380 <khushdilansari345@gmail.com>
2025-09-16 05:36:44 +0530

    added 3

--- frontend/src/pages/Join.jsx
+++ frontend/src/pages/Join.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import FAQ from '../components/FAQ/FAQ';
 import PageLayout from './_layout/PageLayout';
 import AuthCard from '../components/Auth/AuthCard';
 
 const Join = () => (
   <PageLayout>
     <AuthCard />
+     <FAQ />
   </PageLayout>
+ 
 );
 
 export default Join;
